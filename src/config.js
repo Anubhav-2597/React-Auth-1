e36eaca7c3927704453751492fa5011815139cc5
anu2-at-854414645442 <anubhavajmera25@gmail.com>
2022-09-22 06:40:19 +0530

    created an application that adds azure ad authentication to a React App using MSAL azure library using class based methods

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId: '6ff3f37c-652f-4af1-b176-c7171f5d074b',
+    redirectUri: 'http://localhost:3001',
+    scopes:[
+        'user.read'
+    ],
+    authority: 'https://login.microsoftonline.com/580a8b43-4064-4530-9e28-d0e9708ededa'
+};
